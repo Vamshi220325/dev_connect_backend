90a0f780ef0ee07203ede9baf29ee9631efaf677
vamshi <yenaganti.vamshi007@gmail.com>
2025-06-19 11:27:05 +0530

    signup api

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 const express=require("express");
+const {connectDB}=require("./config/databse");
+const {User}=require("./models/user")
 const app=express();
-app.listen(3000,()=>{
+
+app.use(express.json());
+app.post("/signup",async (req,res)=>{
+    
+   
+    const user=new User(req.body);
+    try{
+        await user.save();
+    res.send("user added successfully");
+    }catch(err){
+        res.status(404).send("Error in Saving User"+err.message);
+    }
+    
+})
+
+
+
+
+
+
+connectDB().then(()=>{
+    console.log("db connected successfull");
+    app.listen(3000,()=>{
     console.log("listening on port 3000")
 });
-app.use("/test",(req,res)=>{
-    res.send("hello from test");
-})
-app.use("/",(req,res)=>{
-    res.send("hello from server");
-})
+ }).catch(err=>{
+    console.error("error")
+ })
+
+
+
+
+
+
+
